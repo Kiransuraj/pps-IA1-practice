84f62ed341dbb52c93046936d4cf8cb456fef069
Kiransuraj <96763670+Kiransuraj@users.noreply.github.com>
2022-02-01 06:35:25 +0000

    Completed

--- p1original.c
+++ p1original.c
@@ -1,2 +1,24 @@
-
+#include<stdio.h>
+void input(int *a, int *b)
+{
+  printf("Enter the number:");
+  scanf("%d",a);
+   printf("Enter the number:");
+  scanf("%d",b);
+}
+void add(int a, int b, int *sum)
+{
+  *sum=a+b;
+}
+void output(int a, int b, int sum)
+{
+  printf("The sum of %d + %d is %d",a,b,sum);
+}
+int main()
+{
+  int a,b,sum;
+  input(&a,&b);
+  add(a,b,&sum);
+  output(a,b,sum);
+  return 0;
 }
---
+++ p2original.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int input()
+{
+  printf("Enter the number:")
+}
