0895a29059ed8a1d2df5cc699ce95ca1c95517c6
Kiransuraj <96763670+Kiransuraj@users.noreply.github.com>
2022-02-01 07:27:14 +0000

    Completed

--- p2original.c
+++ p2original.c
@@ -1,5 +1,37 @@
-#include<stdio.h>
+#include <stdio.h>
 int input()
 {
-  printf("Enter the number:")
+  int a;
+  printf("Enter the value\n");
+  scanf("%d",&a);
+  return a;
+}
+int cmp(int a,int b,int c)
+{
+  if((a>b)&&(a>c))
+  {
+    return a;
+  }
+  else if((b>a)&&(b>c))
+  {
+    return b;
+  }
+  else
+  {
+    return c;
+  }
+}
+int output(int cmp)
+{
+  printf("The largest number is %d\n",cmp);
+}
+int main()
+{
+  int a,b,c,Ans;
+ a=input();
+ b=input();
+ c=input();
+ Ans=cmp(a,b,c);
+ output(Ans);
+ return 0;
 }
