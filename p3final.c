b8be770a984bf8b13c91c3d42a6be7fcee713274
Kiransuraj <96763670+Kiransuraj@users.noreply.github.com>
2022-02-01 07:54:28 +0000

    Completed

---
+++ p3final.c
@@ -0,0 +1,28 @@
+int input_n()
+{
+  int n;
+  printf("Enter the value\n");
+  scanf("%d",&n);
+  return n;
+}
+int sum_n(int n)
+{
+  int i,sum=0;
+  for(i=1;i<=n;i++)
+  {
+    sum=sum+i;
+  }
+  return sum;
+}
+void output(int sum)
+{
+  printf("Sum is %d",sum);
+}
+int main()
+{
+  int n,sum;
+  n=input_n();
+  sum=sum_n(n);
+  output(sum);
+  return 0;
+}
